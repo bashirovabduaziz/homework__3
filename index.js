e199c5bc8a07878591d4bd0bf15d819c279381af
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2023-02-23 22:52:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// homework-2
+// let birthYear=+prompt('tugilgan yilingizni yozing');
+// 
+// if(birthYear>2000){
+    // console.log('siz 21 asrda tugilgansiz');
+// }else{
+    // console.log('siz 20 asrda tugilgansiz');
+// }
+
+// let number1=+prompt('birinchi sonni kiriting');
+// let number2=+prompt('ikkinchi sonni kiriting');
+// let allNumbers=+prompt('ikkala sonni qoshing va javobini yozing');
+// 
+// let result=number1+number2;
+// 
+// if(result>=allNumbers){
+    // console.log(`togri ${number1+number2}`);
+// }else{
+    // console.log(`notogri ${number1+number2}`);
+// }
+
+let planeMoney=500;
+let hotelMoney=250;
+let museumMoney=120;
+
+let result=planeMoney+hotelMoney+museumMoney;
+
+let myMoney=+prompt('sizda nechpul bor');
+
+if(myMoney>result){
+    console.log('yaxshi boring');
+}else{
+    console.log('sabr qiling');
+}
